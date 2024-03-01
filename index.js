703b85250f5c63b7194f3f5c768cc67e7d268312
ANNA KUZMENKO <anna.kuzmenko120063@gmail.com>
2024-03-01 23:27:31 +0200

    finished logic

--- index.js
+++ index.js
@@ -1,16 +1,42 @@
-const computerChoice = document.getElementById('computer-choice');
+const computerChoiceDisplay = document.getElementById('computer-choice');
 const userChoiceDisplay = document.getElementById('user-choice');
 const resultDisplay = document.getElementById('result');
 const possibleChoices = document.querySelectorAll('button');
 let userChoice;
+let result;
+
+const choices = [
+  'rock',
+  'scissors',
+  'paper',
+]
 
 possibleChoices.forEach(possibleChoice => possibleChoice.addEventListener('click', (e) => {
   userChoice = e.target.id;
   userChoiceDisplay.innerHTML = userChoice;
   generateComputerChoice();
+  getResult();
 }))
 
 function generateComputerChoice() {
-  let randomNumber = Math.floor(Math.random() * 3 + 1);
-  console.log(randomNumber);
+  const randomNumber = Math.floor(Math.random() * choices.length);
+  computerChoice = choices[randomNumber];
+  computerChoiceDisplay.innerHTML = computerChoice;
+}
+
+function getResult () {
+  if (computerChoice == userChoice) {
+    result = 'its a draw';
+  } else if((computerChoice == "rock" && userChoice == "paper") ||
+     (computerChoice == "paper" && userChoice == "scissors") ||
+     (computerChoice == "scissors" && userChoice == "rock")
+  ) {
+    result = 'you won!';
+  } else {
+    result = 'you lost!'
+  }
+  console.log(computerChoice)
+  console.log(userChoice)
+
+  resultDisplay.innerHTML = result;
 }
